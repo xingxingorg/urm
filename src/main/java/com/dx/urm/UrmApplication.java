200a8ba092b3453f7df3d75c761caa0dfba5b589
duxingxing <820967275@qq.com>
2018-07-22 22:22:24 +0800

    创建项目

---
+++ src/main/java/com/dx/urm/UrmApplication.java
@@ -0,0 +1,12 @@
+package com.dx.urm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrmApplication.class, args);
+	}
+}
